0848a8c76be46e8ce20773bc263de7bafe4e04dc
Grigore Stefan <g_stefan@yahoo.com>
2020-12-04 13:55:37 +0200

    update

--- port/build.platform.github-release-check.cmd
+++ port/build.platform.github-release-check.cmd
@@ -5,7 +5,7 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-echo -^> git-release-check magnet
+echo -^> github-release-check magnet
 
 set PROJECT=magnet
 if not exist source\%PROJECT%.version.ini echo Error - no version & exit 1
